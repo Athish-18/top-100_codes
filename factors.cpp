ca032abe7b5fa8c9c4f3246a2e0f9cb83c2d8c43
Athish <cathishk@gmail.com>
2025-07-17 22:39:03 +0530

    added code

---
+++ factors.cpp
@@ -0,0 +1,26 @@
+vector<int> printDivisors(int n)
+{
+  vector<int> divisors;
+
+  // Note that this loop runs till square root
+  for (int i = 1; i <= sqrt(n); i++)
+  {
+    if (n % i == 0)
+    {
+
+      // If divisors are equal, print only one
+      if (n / i == i)
+      {
+        divisors.push_back(i);
+      }
+      // Otherwise print both
+      else
+      {
+        divisors.push_back(i);
+        divisors.push_back(n / i);
+      }
+    }
+  }
+
+  return divisors;
+}
